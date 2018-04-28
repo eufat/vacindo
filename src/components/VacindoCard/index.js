0950005a705e547eb6f70c2e4f42d0af6c1c6a32
Rian Wardana <gmail@rianwardana.com>
2018-04-28 17:26:53 +0700

    rian

--- src/components/VacindoCard/index.js
+++ src/components/VacindoCard/index.js
@@ -7,12 +7,21 @@ import Typography from 'material-ui/Typography';
 
 const styles = {
   card: {
+    padding: 0,
     maxWidth: 345,
+    fontFamily: "'Enriqueta', arial, serif"
   },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
+  kota: {
+    fontSize: '0.7em'
+  },
+  judul: {
+    fontFamily: "'Enriqueta', arial, serif",
+    fontSize: '1em'
+  }
 };
 
 function VacindoCard(props) {
@@ -22,11 +31,11 @@ function VacindoCard(props) {
       <Card className={classes.card}>
         <CardMedia className={classes.media} image={props.cardImage} title={props.cardTitle} />
         <CardContent>
-          <Typography gutterBottom variant="headline" component="h3">
-            {props.miniHeadline}
+          <Typography gutterBottom variant="headline" component="h3" className={classes.kota}>
+            <b>{(props.miniHeadline).toUpperCase()}</b>
           </Typography>
-          <Typography gutterBottom variant="headline" component="h2">
-            {props.mainHeadline}
+          <Typography gutterBottom variant="headline" component="h2" className={classes.judul}>
+            <b>{props.mainHeadline}</b>
           </Typography>
           <Typography component="p">{props.priceRange}</Typography>
         </CardContent>
