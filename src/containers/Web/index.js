637aee78f512a53972ba507c936bcdc57352ebb3
Rian Wardana <gmail@rianwardana.com>
2018-08-20 19:54:13 +0700

    Search bar berhasil

--- src/containers/Web/index.js
+++ src/containers/Web/index.js
@@ -12,6 +12,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
+import dummy from '../../utils/dummy.json';
+
 // Algolia
 var algoliasearch = require('algoliasearch');
 var algoliasearch = require('algoliasearch/reactnative');
@@ -20,118 +22,9 @@ var algoliasearch = require('algoliasearch/lite');
 var clientAlgolia = algoliasearch('VR7FD3DTUW', 'b4e4ff31927c35e087ef52d93784f081');
 var index = clientAlgolia.initIndex('index');
 
-index.search('puppet', function(err, content) {
-  console.log(content.hits);
-});
-
 // Program
-const data = [
-  {
-    "imageURL":
-      "http://static.asiawebdirect.com/m/bangkok/portals/indonesia-holidays-com/homepage/samosir-island/allParagraphs/BucketComponent/ListingContainer/01/image/samo600b.jpg",
-    "url": "http://www.indonesia.travel/en/destination/point-of-interest/samosir-island",
-    "color": null,
-    "preTitle": "Lakes",
-    "title": "Samosir Island",
-    "buildings": null,
-    "stars": null,
-    "subtitle": null,
-    "description":
-      "The island of Samosir, situated in the huge crater lake of Toba. The island, together with its surrounding areas is the heart of the Toba Batak culture. A visit to Lake Toba is not complete without a stay on Samosir with its many traditional villages along its shoreline.",
-    "places": null,
-    "pois": null,
-    "longitude": "98.792772",
-    "latitude": "2.640869",
-    "price": "1500000"
-  },
-  {
-    "imageURL": "http://farm6.staticflickr.com/5499/11509522905_f4c10137e1_b.jpg",
-    "url": "http://www.indonesia.travel/en/destination/point-of-interest/sigale-gale-puppet-show",
-    "color": null,
-    "preTitle": "Arts & Cultures",
-    "title": "Sigale-gale Puppet Show",
-    "buildings": null,
-    "stars": null,
-    "subtitle": null,
-    "description":
-      " Sigale-gale is a Batak life-sized puppet show, which is traditionally performed at funerals but may now also be performed for visitors.",
-    "places": null,
-    "pois": null,
-    "longitude": "98.863841",
-    "latitude": "2.612432",
-    "price": "1500000"
-  },
-  {
-    "imageURL": "https://images2.tempo.co/data/2014/06/28/id_302262/302262_620.jpg",
-    "url": "http://www.indonesia.travel/en/destination/point-of-interest/oukup",
-    "color": null,
-    "preTitle": "Spas",
-    "title": "Oukup",
-    "buildings": null,
-    "stars": null,
-    "subtitle": null,
-    "description":
-      "The Batak ethnic groups living in North Sumatera have long realized that nature is not only man\u2019s  main source of life but it also provides precious medicinal cures. Traditional natural treatments have long been part of daily life in North Sumatera.",
-    "places": null,
-    "pois": null,
-    "longitude": "98.620011",
-    "latitude": "95.336521",
-    "price": "1500000"
-  },
-  {
-    "imageURL": "http://kemanaaja.com/wp-content/uploads/2015/11/IMG_4501-825x510.jpg",
-    "url": "http://www.indonesia.travel/en/destination/point-of-interest/bukit-lawang",
-    "color": null,
-    "preTitle": "Forests & Wildlife",
-    "title": "Bukit Lawang",
-    "buildings": null,
-    "stars": null,
-    "subtitle": null,
-    "description":
-      "Bukit Lawang is a small village situated at the south of the Mount Leuseur National Park and the gateway to the legendary Sumatran jungle.",
-    "places": null,
-    "pois": null,
-    "longitude": "98.144269",
-    "latitude": "3.555769",
-    "price": "1500000"
-  },
-  {
-    "imageURL":
-      "https://i2.wp.com/pamitrantours.com/wp-content/uploads/2016/04/Pulau-Tidung-3.jpg?fit=885%2C584",
-    "url": "http://www.indonesia.travel/en/destination/point-of-interest/tidung-island",
-    "color": null,
-    "preTitle": "Beaches",
-    "title": "Tidung Island",
-    "buildings": null,
-    "stars": null,
-    "subtitle": null,
-    "description":
-      "Located relatively close to the capital city of Jakarta, you wouldn\u2019t have to spend unnecessary time and money just trying to get to Tidung Island. For Jakarta-dwellers looking for a simple and affordable weekend getaway, Tidung offers secluded beaches, starry nights and an amiable community, just a stone\u2019s throw away from the capital.",
-    "places": null,
-    "pois": null,
-    "longitude": "106.496018",
-    "latitude": "-5.797094",
-    "price": "1500000"
-  },
-  {
-    "imageURL":
-      "https://i0.wp.com/www.balistarisland.com/wp-content/uploads/2016/10/bidadariislandkomodo.jpg?fit=1200%2C650&ssl=1",
-    "url": "http://www.indonesia.travel/en/destination/point-of-interest/bidadari-island",
-    "color": null,
-    "preTitle": "Beaches",
-    "title": "Bidadari Island",
-    "buildings": null,
-    "stars": null,
-    "subtitle": null,
-    "description":
-      "The island gained its name because long ago, no one was allowed to visit the island unless they were of royal blood of Jayakarta. Hence the name, Pulau Bidadari, literally translated meaning The Island of Angels. With only 6 hectares in area, Pulau Bidadari is a small island. You could walk from one end to the other in under two hours. The beaches of Pulau Bidadari display beautiful, white, sand, and crystal clear sea water.",
-    "places": null,
-    "pois": null,
-    "longitude": "106.746941",
-    "latitude": "-6.035613",
-    "price": "1500000"
-  }
-];
+let { data } = dummy;
+data = data.slice(0,6);
 
 
 
@@ -168,12 +61,23 @@ const styles = theme => ({
 
 
 class Web extends Component {
+  state = {
+    data: data
+  }
+
+  handleSearch = (value) => {
+    index.search(value, (err, content) => {
+    const newData = content.hits;
+    this.setState({ ...this.state, data: newData});
+    
+  })}
+
   render() {
     const {classes} = this.props;
     const {history} = this.props;
 
     let Cards = [];
-    data.forEach(item => {
+    this.state.data.forEach(item => {
       Cards.push(
         <Grid item xs={12} sm={4}>
           <VacindoCard data={item} />
@@ -207,11 +111,7 @@ class Web extends Component {
         <div className={this.props.classes.searchBarContainer}>
           <SearchBar
             onChange={() => console.log('onChange')}
-            onRequestSearch={(value) => {
-            	index.search(value, function(err, content) {
-				  alert(JSON.stringify(content.hits[0]));
-				});
-            }}
+            onRequestSearch={(value) => this.handleSearch(value)}
             style={{
               margin: '0 auto',
               maxWidth: 800
