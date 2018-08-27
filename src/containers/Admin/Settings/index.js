ea8149d011af1ddbc7b64b2490b29307dc90a20a
Eufrat Tsaqib <eutsat@gmail.com>
2018-08-27 10:51:05 +0700

    Add admin settings page

---
+++ src/containers/Admin/Settings/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+
+class Settings extends Component {
+    render() {
+        return (
+            <div>
+                This is Settings page
+            </div>
+        );
+    }
+}
+
+export default Settings;
